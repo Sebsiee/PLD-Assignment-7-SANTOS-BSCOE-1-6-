f639b4ed906a92c908a4c3c94fb028a2c120fd2a
Sebastian <santosbasty4@gmail.com>
2021-11-27 22:33:49 +0800

    Third Commit - Validation for Capital Vowels

--- Word Counter.py
+++ Word Counter.py
@@ -6,11 +6,13 @@ def wordCounter():
     vowels = 0
     consonants = 0
     for i in sentence:
-        if(i == "a" or i == "e" or i == "i" or i == "o" or i == "u"):
+        if(i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U"):
             vowels = vowels+1
         else:
             consonants = consonants+1
     print ("Output: " +  sentence)
+    countWords = len(sentence.split())
+    print ("Words: " +  str(countWords))
     print("Vowels:", vowels)
     print("Consonants:", consonants)
 
