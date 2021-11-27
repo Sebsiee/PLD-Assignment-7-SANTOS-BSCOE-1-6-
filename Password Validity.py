4455e2149c3cb7f9e806ad61e3da78d90d4070f8
Sebastian <santosbasty4@gmail.com>
2021-11-27 22:31:14 +0800

    Second Commit - Imported Regular Expression, Capital & Number Identifier

--- Password Validity.py
+++ Password Validity.py
@@ -1,3 +1,5 @@
+import re
+
 def introduction():
     print("Welcome to my Program 2 - Password Validity!")
 
@@ -8,6 +10,12 @@ def passwordValidation():
         if (len(password)<=15):
             validator = +1
             print("Your password should be greater than 15 characters")
+        elif not re.search("[A-Z]", password):
+            validator = +1
+            print("Your password should have a capital letter.")
+        elif not re.search("[0-9]", password):
+            validator = +1
+            print("Your password should have a number.")
         else:
             validator = 0
             print("Output: Valid Password")
