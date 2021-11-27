0da861f07ef0337fd5f4e14420bde25debded158
Sebastian <santosbasty4@gmail.com>
2021-11-27 22:21:23 +0800

    First Commit - Base

---
+++ Word Counter.py
@@ -0,0 +1,18 @@
+def introduction():
+    print("Welcome to my Program 1 - Word Counter!")
+
+def wordCounter():
+    sentence = input("Input: ")
+    vowels = 0
+    consonants = 0
+    print ("Output: " +  sentence)
+    print("Vowels:", vowels)
+    print("Consonants:", consonants)
+
+def goodbye():
+    print()
+    print("Thank you for using my program!")
+
+introduction()
+wordCounter()
+goodbye()
